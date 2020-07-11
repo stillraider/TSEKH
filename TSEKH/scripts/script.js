0c46e8f5909628bbfa6f146d1b155225bb32a81c
stillraider <stilryder74@mail.ru>
2020-07-11 18:29:05 +0500

    completed the page. with a gulp
    Completed the site page with full optimization and adaptation of the site

--- TSEKH/scripts/script.js
+++ TSEKH/scripts/script.js
@@ -1,46 +1,22 @@
-let burger = $(".header-mobile__burger");
-let burger_first = $(".header-mobile__open-menu:first-child");
-let burger_last = $(".header-mobile__open-menu:last-child");
-let burger_center = $(".header-mobile__open-menu:nth-child(2)");
-let bnt = $(".down-menu__dropdown-button");
-let dropdown = $(".down-menu__dropdown-content");
-let footer_links = $(".down-menu__footer-links");
-let down_items = $(".down-menu__list-items");
-let down_menu = $(".down-menu");
+let navWrap = $(".header-wrap");
+let navBlocks = $(".header__burger");
+let headerJackdaw = $(".header-wrap__jackdaw");
 let html = $("html");
 
-burger.on("click",
+navBlocks.on("click", 
     function () {
-        burger_first.toggleClass("bias_first");
-        burger_last.toggleClass("bias_last");
-        burger_center.toggleClass("bias_center");
-        down_menu.toggleClass("menu-show");
-        html.toggleClass("scroll");
-        show_dropdown ();
+        navWrap.show();
+        html.css("overflow", "hidden");
     }
-);
-function show_dropdown () {
-    dropdown.css("left", "100vw");
-    down_items.show();
-    footer_links.show();
-}
-down_items.on("click", 
+)
+headerJackdaw.on("click", 
     function () {
-        down_index = down_items.index(this);
-        down_items.hide();
-        down_items.eq(down_index).show();
-        dropdown.eq(down_index).css("left", "0");
-        footer_links.hide();
-        dropdown.css("margin-bottom", "40px");
+        navWrap.hide();
+        html.css("overflow", "inherit");
     }
-);
-bnt.on("click", 
-    show_dropdown
-);
+)
 
-$(function(){
-    $("#phone").mask("+7 (999) 999-9999");
-})
+$(".phone").mask("+7 (999) 999-9999");
 
 
 
